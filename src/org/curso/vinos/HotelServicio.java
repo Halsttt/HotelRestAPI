1dbdac6be1c39f225007ca1b1302e2959cde5be6
Toni  Torres <tonitorres@MacBook-Pro-de-Toni.local>
2018-05-10 17:04:54 +0200

    Clase, DAO, SERVICE AND HOTEL UPDATED

---
+++ src/org/curso/vinos/HotelServicio.java
@@ -0,0 +1,62 @@
+package org.curso.vinos;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/vinos")
+public class HotelServicio {
+
+	HotelDAO dao = new HotelDAO();
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Hotel> findAll() {		
+		return dao.findAll();
+	}
+
+	@GET @Path("search/{query}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Hotel findByName(@PathParam("query") String query) {		
+		return dao.findByName(query).get(0);
+	}
+
+	@GET @Path("{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Hotel findById(@PathParam("id") int id) {
+		System.out.println("findById " + id);
+		return dao.findById(id);
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Hotel create(Hotel hotel) {
+		System.out.println("creando vino");
+		return dao.save(hotel);
+	}
+
+	@PUT @Path("{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Hotel update(Hotel hotel, @PathParam("id") int id) {		
+		System.out.println("Actualizando vino: " + hotel.getNombre());
+		hotel.setId(id);
+		dao.save(hotel);
+		return hotel;
+	}
+	
+	@DELETE @Path("{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public void remove(@PathParam("id") int id) {
+		dao.remove(id);
+	}
+}
